29f613ff6cf1927152ab71bd45e6cd50fb836e63
Ankita Banerjee <banerjeeankita403@gmail.com>
2025-06-25 01:24:46 +0530

    Add production build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
-import Projects from "./components/Projects";
+import Projects from "./components/projects";
 
 
 
